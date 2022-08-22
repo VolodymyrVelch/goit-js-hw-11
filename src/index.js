98f666e4ecd1224394f463a12232cf1fff960b82
Volodymyr Velch <velch83@gmail.com>
2022-08-22 20:46:05 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const NOTIFY_OPTIONS = {
+  timeout: 1000,
+  showOnlyTheLastOne: true,
+  clickToClose: true,
+};
