362ab5f0efa4f4f41e9dcf07c97363d70e25204b
Volodymyr Velch <velch83@gmail.com>
2022-08-22 22:07:43 +0300

    npm-modules-added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const NOTIFY_OPTIONS = {
   timeout: 1000,
   showOnlyTheLastOne: true,
   clickToClose: true,
 };
+
+// https://pixabay.com/api/
